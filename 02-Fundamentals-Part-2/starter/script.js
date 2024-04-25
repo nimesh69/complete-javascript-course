06178de8f9e92ad4cb4641253a0f7a066981e521
Nimesh <nimeshstha79@gmail.com>
2024-04-25 09:27:12 +0545

    dot vs bracket notation

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -188,4 +188,36 @@ const jonas ={
 
 
 
+const jonas ={
+    firstName: 'jonas',
+    lastName: 'Schmedtmann',
+    age: 2037-1991,
+    job: 'teacher',
+    friends: ['Michael', 'Peter', 'Steven']
+} 
+console.log(jonas);
 
+console.log(jonas.lastName);
+console.log(jonas['lastName']);
+
+const nameKey = 'Name';
+console.log(jonas['first'+nameKey]); 
+console.log(jonas['last'+nameKey]);
+// console.log(jonas[])
+// console.log(jonas.'last'+nameKey);
+
+const interestedIn = prompt('What do you want to know about jonas? choose between firstName,lastName,age,job, and friends');
+
+if (jonas[interestedIn]){
+    console.log(jonas[interestedIn]);
+}
+else{
+    console.log('Wrong request! What do you want to know about jonas? choose between firstName,lastName,age,job, and friends')
+}
+
+jonas.location = 'Portugal';
+jonas['twitter'] = '@jonasschmedtman';
+console.log(jonas);
+
+// delete jonas.twitter;
+console.log(`${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]}`)
